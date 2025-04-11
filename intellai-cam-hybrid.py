71a9413cfd4add0b5745f164f6b0b309b3d8eb0f
MGBrebu <mgbrebu@proton.me>
2025-04-11 09:39:47 +0100

    move model code to classes, shared defs in utils

--- intellai-cam-hybrid.py
+++ intellai-cam-hybrid.py
@@ -3,155 +3,125 @@ import json
 import os
 import datetime
 from deepface import DeepFace
-from time import perf_counter
+
+from utils.model_utils import open_cam, save_analysis
+from utils.timer import Timer
 
 # -----------------------------------
 
-# Open the default camera (0) using OpenCV
-def open_cam(cam_id=0):
-    try:
-        cam = cv2.VideoCapture(cam_id)
-        if not cam.isOpened():
-            print("Open Cam Error: Could not open camera.")
-            return None
-    except cv2.error as e:
-        print("OpenCV Error:", e)
-        return None
-    return cam
+class HybridModel:
+    # Extracts faces from a frame using OpenCV Haar cascades
+    def extract(self, frame):
+        try:
+            # Load OpenCV Haar cascade
+            face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
 
-# Extracts faces from a frame using OpenCV Haar cascades
-def extract(frame):
-    try:
-        # Load OpenCV Haar cascade
-        face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+            # Convert to grayscale for Haar detection
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-        # Convert to grayscale for Haar detection
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            # Detect faces
+            faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
 
-        # Detect faces
-        faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
-
-        for (x, y, w, h) in faces:
-            # Draw rectangle around face
-            cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
-
-            # Crop face region
-            faces_coords = (x, y, w, h)
-            face_img = frame[y:y+h, x:x+w]
-
-            return face_img, faces_coords
-        return None, None
-    except Exception as e:
-        print("Extraction Error:", e)
-        return None, None
-
-# Analyse a frame for faces and their facial attributes (age, gender, race) using DeepFace
-def analyse(frame):
-    try: 
-        analysis = DeepFace.analyze(
-            frame,
-            actions = ['age', 'gender', 'race'],
-            detector_backend = 'mtcnn',
-            enforce_detection=False
-        )
-        return analysis
-    except Exception as e:
-        print("Analysis Error:", e)
-        analysis = {}
-
-# Save an analysis result to a JSON file
-# Creates JSON file if it doesn't exist, otherwise appends
-def save_analysis(result, output_file='./analysis/hybrid_face_analysis.json'):
-    # Save selected fields + timestamp
-    if not os.path.exists(output_file):
-        with open(output_file, "w") as f:
-            f.write("[]\n")
-
-    trimmed_analysis = {
-        "timestamp": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), 
-        "age": result.get("age", "Unknown"), "gender": result.get("dominant_gender", "Unknown"), 
-        "race": result.get("dominant_race", "Unknown")
-    }
-
-    try:
-        with open(output_file, "r+") as f:
-            data = json.load(f)
-            data.append(trimmed_analysis)
-            f.seek(0)
-            json.dump(data, f, indent=2)
-            f.truncate()
-        print("Saved:", trimmed_analysis)
-    except Exception as e:
-        print("Save Analysis Error:", e)
-
-# Uses above functions to open the camera, read frames, and analyse faces
-def run_model(framerate=24, frequency=24, cam_ids=[0]):
-    print("----------------------")
-    print("Running Model - HYBRID")
-    print("----------------------")
-
-    total_start_time = perf_counter()
-    frame_count = 0
-    analysis_times = []
-
-    cams = [open_cam(cam_id) for cam_id in cam_ids]
-    if None in cams:
-        print("Run Model Error: One or more cameras could not be opened.")
-        return
-    if not cams:
-        print("Run Model Error: No cameras available.")
-        return
-
-    while True:
-        for cam in cams:
-            ret, frame = cam.read()
-            if not ret:
-                print("Run Model Error: Could not read frame from camera.")
-                continue
-
-            frame_count += 1
-
-            if frame_count % frequency == 0:
-                start_analysis = perf_counter()
-
-                face_img, faces_coords = extract(frame)
+            for (x, y, w, h) in faces:
                 # Draw rectangle around face
-                if face_img is not None:
-                    x, y, w, h = faces_coords
-                    cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-                    analysis = analyse(face_img)
+                cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
 
-                    if isinstance(analysis, list) and len(analysis) > 0:
-                        result = analysis[0]
-                        save_analysis(result)
+                # Crop face region
+                faces_coords = (x, y, w, h)
+                face_img = frame[y:y+h, x:x+w]
 
-                end_analysis = perf_counter()
-                analysis_duration = end_analysis - start_analysis
-                analysis_times.append(analysis_duration)
-                print(f"[Frame {frame_count}] Analysis Time: {analysis_duration:.4f} sec")
-            
-            cv2.imshow(f"Camera Feed {cam}", frame)
+                return face_img, faces_coords
+            return None, None
+        except Exception as e:
+            print("Extraction Error:", e)
+            return None, None
 
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-    
-    total_end_time = perf_counter()
-    total_time = total_end_time - total_start_time
+    # Analyse a frame for faces and their facial attributes (age, gender, race) using DeepFace
+    def analyse(self, frame):
+        try: 
+            analysis = DeepFace.analyze(
+                frame,
+                actions = ['age', 'gender', 'race'],
+                detector_backend = 'mtcnn',
+                enforce_detection=False
+            )
+            return analysis
+        except Exception as e:
+            print("Analysis Error:", e)
+            analysis = {}
 
-    cam.release()
-    cv2.destroyAllWindows()
+    # Uses above functions to open the camera, read frames, and analyse faces
+    def run_model(self, framerate=24, frequency=24, cam_ids=[0]):
+        print("----------------------")
+        print("Running Model - HYBRID")
+        print("----------------------")
 
-    # Performance Summary
-    print(f"\nTotal Frames: {frame_count}")
-    print(f"Total Elapsed Time: {total_time:.2f} sec")
-    print(f"Average Analysis Time: {sum(analysis_times)/len(analysis_times):.4f} sec")
-    print(f"Estimated FPS (including analysis): {frame_count / total_time:.2f}")
+        # Initialise timers
+        total_timer = Timer(label="Total")
+        analysis_timer = Timer(label="Analysis")
+
+        total_timer.start()
+
+        frame_count = 0
+
+        cams = [open_cam(cam_id) for cam_id in cam_ids]
+        if None in cams:
+            print("Run Model Error: One or more cameras could not be opened.")
+            return
+        if not cams:
+            print("Run Model Error: No cameras available.")
+            return
+        
+        for cam in cams:
+            cam.set(cv2.CAP_PROP_FPS, framerate)
+
+        while True:
+            for cam in cams:
+                ret, frame = cam.read()
+                if not ret:
+                    print("Run Model Error: Could not read frame from camera.")
+                    continue
+
+                frame_count += 1
+
+                if frame_count % frequency == 0:
+                    analysis_timer.start()
+
+                    face_img, faces_coords = self.extract(frame)
+                    # Draw rectangle around face
+                    if face_img is not None:
+                        x, y, w, h = faces_coords
+                        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+                        analysis = self.analyse(face_img)
+
+                        if isinstance(analysis, list) and len(analysis) > 0:
+                            result = analysis[0]
+                            save_analysis(result)
+
+                    analysis_timer.stop()
+                
+                cv2.imshow(f"Camera Feed {cam}", frame)
+
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+        
+        total_timer.stop()
+
+        cam.release()
+        cv2.destroyAllWindows()
+
+        # Performance Summary
+        print("\n====== Performance Summary ======")
+        print(f"\nProcessed Frames: {frame_count}")
+        total_timer.summary(False, False)
+        analysis_timer.summary()
 
 # -----------------------------------
 
-# Main for testing
+# Main
 def main():
-    run_model()
+    model = HybridModel()
+    model.run_model(framerate=24, frequency=24, cam_ids=[0])
 
 # -----------------------------------
 
--- intellai-cam-single.py
+++ intellai-cam-single.py
@@ -3,133 +3,106 @@ import json
 import os
 from deepface import DeepFace
 import datetime
-from time import perf_counter
+
+from utils.model_utils import open_cam, save_analysis
+from utils.timer import Timer
+
 
 # -----------------------------------
 
-# Open the default camera (0) using OpenCV
-def open_cam(cam_id=0):
-    try:
-        cam = cv2.VideoCapture(cam_id)
-        if not cam.isOpened():
-            print("Open Cam Error: Could not open camera.")
-            return None
-    except cv2.error as e:
-        print("OpenCV Error:", e)
-        return None
-    return cam
+class SingleModel:
+    # Deprecated: analyse() includes face detection
+    # Extracts faces from a frame using DeepFace
+    def extract(self, frame):
+        try:
+            faces = DeepFace.extract_faces(frame, detector_backend = 'mtcnn')
+            return faces
+        except Exception as e:
+            print("Extraction Error:", e)
+            return []
 
-# Deprecated: analyse() includes face detection
-# Extracts faces from a frame using DeepFace
-def extract(frame):
-    try:
-        faces = DeepFace.extract_faces(frame, detector_backend = 'mtcnn')
-        return faces
-    except Exception as e:
-        print("Extraction Error:", e)
-        return []
+    # Analyse a frame for faces and their facial attributes (age, gender, race) using DeepFace
+    def analyse(self, frame):
+        try: 
+            analysis = DeepFace.analyze(
+                frame,
+                actions = ['age', 'gender', 'race'],
+                detector_backend = 'mtcnn'
+            )
+            return analysis
+        except Exception as e:
+            print("Analysis Error:", e)
+            analysis = {}
 
-# Analyse a frame for faces and their facial attributes (age, gender, race) using DeepFace
-def analyse(frame):
-    try: 
-        analysis = DeepFace.analyze(
-            frame,
-            actions = ['age', 'gender', 'race'],
-            detector_backend = 'mtcnn'
-        )
-        return analysis
-    except Exception as e:
-        print("Analysis Error:", e)
-        analysis = {}
+    # Uses above functions to open the camera, read frames, and analyse faces
+    def run_model(self, framerate=24, frequency=24, cam_ids=[0]):
+        print("----------------------")
+        print("Running Model - SINGLE")
+        print("----------------------")
 
-# Save an analysis result to a JSON file
-# Creates JSON file if it doesn't exist, otherwise appends
-def save_analysis(result, output_file='./analysis/single_face_analysis.json'):
-    # Save selected fields + timestamp
-    if not os.path.exists(output_file):
-        with open(output_file, "w") as f:
-            f.write("[]\n")
+        # Initialise timers
+        total_timer = Timer(label="Total")
+        analysis_timer = Timer(label="Analysis")
 
-    trimmed_analysis = {
-        "timestamp": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), 
-        "age": result.get("age", "Unknown"), "gender": result.get("dominant_gender", "Unknown"), 
-        "race": result.get("dominant_race", "Unknown")
-    }
+        total_timer.start()
 
-    try:
-        with open(output_file, "r+") as f:
-            data = json.load(f)
-            data.append(trimmed_analysis)
-            f.seek(0)
-            json.dump(data, f, indent=2)
-            f.truncate()
-        print("Analysis Saved:", trimmed_analysis)
-    except Exception as e:
-        print("Save Analysis Error:", e)
+        frame_count = 0
 
-# Uses above functions to open the camera, read frames, and analyse faces
-def run_model(framerate=24, frequency=24, cam_ids=[0]):
-    print("----------------------")
-    print("Running Model - SINGLE")
-    print("----------------------")
-
-    total_start_time = perf_counter()
-    frame_count = 0
-    analysis_times = []
-
-    cams = [open_cam(cam_id) for cam_id in cam_ids]
-    if None in cams:
-        print("Run Model Error: One or more cameras could not be opened.")
-        return
-    if not cams:
-        print("Run Model Error: No cameras available.")
-        return
-
-    while True:
+        cams = [open_cam(cam_id) for cam_id in cam_ids]
+        if None in cams:
+            print("Run Model Error: One or more cameras could not be opened.")
+            return
+        if not cams:
+            print("Run Model Error: No cameras available.")
+            return
+        
         for cam in cams:
-            ret, frame = cam.read()
-            if not ret:
-                print("Run Model Error: Could not read frame from camera.")
-                continue
+            cam.set(cv2.CAP_PROP_FPS, framerate)
 
-            frame_count += 1
+        while True:
+            for cam in cams:
+                ret, frame = cam.read()
+                if not ret:
+                    print("Run Model Error: Could not read frame from camera.")
+                    continue
 
-            if frame_count % frequency == 0:
-                start_analysis = perf_counter()
+                frame_count += 1
 
-                analysis = analyse(frame)
+                if frame_count % frequency == 0:
+                    analysis_timer.start()
 
-                if isinstance(analysis, list) and len(analysis) > 0:
-                    result = analysis[0]
-                    save_analysis(result)
+                    analysis = self.analyse(frame)
 
-                end_analysis = perf_counter()
-                analysis_duration = end_analysis - start_analysis
-                analysis_times.append(analysis_duration)
-                print(f"[Frame {frame_count}] Analysis Time: {analysis_duration:.4f} sec")
+                    if isinstance(analysis, list) and len(analysis) > 0:
+                        result = analysis[0]
+                        save_analysis(result)
 
-            cv2.imshow(f"Camera Feed {cam}", frame)
+                    analysis_timer.stop()
+                    
 
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-    
-    total_end_time = perf_counter()
-    total_time = total_end_time - total_start_time
+                cv2.imshow(f"Camera Feed {cam}", frame)
 
-    cam.release()
-    cv2.destroyAllWindows()
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+        
+        total_timer.stop()
 
-    # Performance Summary
-    print(f"\nTotal Frames: {frame_count}")
-    print(f"Total Elapsed Time: {total_time:.2f} sec")
-    print(f"Average Analysis Time: {sum(analysis_times)/len(analysis_times):.4f} sec")
-    print(f"Estimated FPS (including analysis): {frame_count / total_time:.2f}")
+        cam.release()
+        cv2.destroyAllWindows()
+
+        # Performance Summary
+        print("\n====== Performance Summary ======")
+        print(f"\nProcessed Frames: {frame_count}")
+        total_timer.summary(False, False)
+        analysis_timer.summary()
+
 
 # -----------------------------------
 
-# Main for testing
+# Main
 def main():
-    run_model()
+    model = SingleModel()
+    model.run_model(framerate=24, frequency=24, cam_ids=[0])
 
 # -----------------------------------
 
